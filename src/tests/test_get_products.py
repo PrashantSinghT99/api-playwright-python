e14e14656c14ae9481932ed9cff02d4863159f82
Prashant Singh <prashantsinghtariyal@gmail.com>
2024-07-04 13:31:28 +0530

    adding params

--- src/tests/test_get_products.py
+++ src/tests/test_get_products.py
@@ -10,3 +10,14 @@ def test_get_a_product(set_api_context:APIRequestContext):
     response_data=response.json()['title']
     assert response.status == 200,"Request to fetch all products failed"
     assert response_data == "Essence Mascara Lash Princess","Title for product 1 not matching"
+    
+def test_fetch_products_with_params(set_api_context: APIRequestContext):
+    params = {
+        'limit': 10,
+        'skip': 10,
+        'select': 'title,price'
+    }
+    response = set_api_context.get('/products', params=params)
+    assert response.status == 200, "Request to fetch products failed"
+    products = response.json().get('products', [])  
+    assert len(products) == 10 ,"limit and skip not working"
